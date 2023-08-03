2e40d1dd74d5ef55d85aa57b6337f7f9b019bf2c
qspencer0224 <quentonspencer4@gmail.com>
2023-08-03 13:17:26 -0500

    styling

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/app_context'
 import Nav_UI from '../components/Nav_UI'
-import img1 from '.images/cardpic2.png';
 
 function Portfolio() {
 
@@ -12,12 +11,51 @@ function Portfolio() {
         <div className='title'>MANIFESTATIONS</div>
         <div className="artbox">
           <div className="rowone">
-            <div className="picone">
-              <img src={img1} />
+            <div className="r1left">
+            <div className="r1lbox">
+            <div className="pic" id='one'>
+                <img src='' alt='' id='picone' />
+              </div>
+              <div className="pic" id='two'>
+                <img src='' alt='' id='pictwo' />
+              </div>
+            </div>
+            <div className="description" id='done'>This is my Card Project</div>
+            <div className="git" id='gitone'>
+              {/* this is where the github link is going */}
+            </div>
+            </div>
+
+
+            <div className="r1right">
+              <div className="r1rightbox">
+                <div className="pic" id='three'>
+                  <img src='' alt='' id='picthree' />
+                </div>
+              </div>
+              <div className="description" id='dtwo'>This is my ATM Project</div>
+              <div className="git" id='gittwo'>
+                {/* this is where the github link is going */}
+              </div>
             </div>
           </div>
-          <div className="rowtwo"></div>
-          <div className="rowthree"></div>
+
+          <div className="rowtwo">
+            <div className="r2left">
+              <div className="r2leftbox">
+                <div className="pic" id='four'>
+                  <img src='' alt='' id='picfour'/>
+                </div>
+                <div className="description" id='dthree'>This is my Calculator Project</div>
+                <div className="git" id='gitthree'>
+                {/* this is where my github link is going */}
+              </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="rowthree">
+          </div>
         </div>
       </div>
     
