1a4bd1ba7395d839429e442afdc0b9d24b4c8e3f
qspencer0224 <quentonspencer4@gmail.com>
2023-08-05 18:29:21 -0500

    changed home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,35 +9,53 @@ function Home() {
   return (
       <div className='homebackground'>
         <Nav_UI/>
-        <div className='title'>"THE IDEA BOOK"</div>
-        <div className="bigbox">
+        {/* <div className='title'>"THE IDEA BOOK" of quenton spencer</div> */}
+        <div className="homebox">
+          <div className="chaosbox">
+            <img src='https://cdn.britannica.com/24/3824-050-0F72B625/Hippopotamus-Hunt-canvas-Peter-Paul-Rubens-Alte.jpg' alt='' className='chaos' />
+          </div>
+          <div className="homeright">
+            <div className="school">
+              <img src='https://sharecharlotte.org/sites/default/files/Logo-Horizontal-Light.png' alt='' className='schoolpic' />
+            </div>
+            <div className="skills">
+              <img src='https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-devextreme-multi-purpose-controls-html-javascript-3.png' alt='' className='skillpic'/>
+              <img src='https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png' alt='' className='skillpic'/>
+            </div>
+            <div className="instagram">Instagram: @ techinsta</div>
+            <div className="email">Email me: randomemail@gmail.com</div>
+            <div className="logo">
+              <img src='https://cdn-icons-png.flaticon.com/512/4257/4257487.png' alt='' id='logo' />
+            </div>
+          </div>
+        </div>
+        {/* <div className="bigbox">
           <div className="details">
-            <div className='name'>-QUENTON SPENCER</div>
             <div className="creative">-creative</div>
             <div className='engineer'>-software engineer</div>
             <div className='human'>-human.</div>
           </div>
-        </div>
+        </div> */}
           
 
-          <div className="skills">
+          {/* <div className="skills">
             <img src='https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-devextreme-multi-purpose-controls-html-javascript-3.png' alt='' className='skillpic'/>
             <img src='https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png' alt='' className='skillpic'/>
-          </div>
-          <div className="school">
+          </div> */}
+          {/* <div className="school">
             <img src='https://sharecharlotte.org/sites/default/files/Logo-Horizontal-Light.png' alt='' className='schoolpic' />
-          </div>
-          <div className="infobox">
+          </div> */}
+          {/* <div className="infobox">
             <div className="left">
               <div className="instagram">Instagram: @ techinsta</div>
               <div className="email">Email me: randomemail@gmail.com</div>
             </div>
             <div className="right"></div>
-          </div>
+          </div> */}
 
-          <Marquee>
+          {/* <Marquee>
           <div className='marq'>"Random quote here, i'm just typing stuff just for styling purposes" -Quenton Spencer</div>
-          </Marquee>
+          </Marquee> */}
       </div>
   )
 }
