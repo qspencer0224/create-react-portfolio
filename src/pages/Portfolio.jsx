a28b0c08a1a7f016d4574e148d20491943bc01ae
qspencer0224 <quentonspencer4@gmail.com>
2023-08-06 00:49:48 -0500

    testing out api

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,17 +1,34 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../context/app_context'
-import Nav_UI from '../components/Nav_UI'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Portfolio() {
-
-    const {} = useContext(AppContext)
+  const [projects, setProjects] = useState([])
+  useEffect(() => {
+    getProjects()
+  }, [])
+  const getWork = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.github.com/users/qspencer0224/repos'
+      )
+      const firstTenProjects = response.data.slice(0, 10);
+      setProjects(firstTenProjects);
+    } catch (error) {
+      console.error('Error fetching projects:', error);
+    }
+  };
   return (
-      <div className='portBackground'>
-        <Nav_UI/>
-        <div className='title'>MANIFESTATIONS</div>
-      </div>
-    
-  )
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <a href={project.html_url}>{project.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Portfolio
